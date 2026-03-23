1ef97850ebfdfb85bcbcfe823dc93535b3cf9c47
Raissa Mutagoma <rmutagoma@gmail.com>
2026-03-23 11:12:39 +0100

    Fix formatting of JwtService class declaration

--- project-service/src/main/java/com/budgetalert/projectservice/security/JwtService.java
+++ project-service/src/main/java/com/budgetalert/projectservice/security/JwtService.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey; 
 import java.nio.charset.StandardCharsets; 
 import java.util.Date;
-@Service public class JwtService {
+@Service
+ public class JwtService {
  private final String secret;
  private final long expirationMs;
  private SecretKey key;
