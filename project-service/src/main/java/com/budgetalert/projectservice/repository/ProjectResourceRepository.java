9a91a54e0cf17fa72fb44272345da3f747d2d546
Raissa Mutagoma <rmutagoma@gmail.com>
2026-03-23 10:44:46 +0100

    Fix formatting and add newlines in ProjectResourceRepository

--- project-service/src/main/java/com/budgetalert/projectservice/repository/ProjectResourceRepository.java
+++ project-service/src/main/java/com/budgetalert/projectservice/repository/ProjectResourceRepository.java
@@ -1,1 +1,4 @@
-package com.budgetalert.projectservice.repository; import com.budgetalert.projectservice.model.ProjectResource; import org.springframework.data.jpa.repository.JpaRepository; public interface ProjectResourceRepository extends JpaRepository<ProjectResource,String>{}
+package com.budgetalert.projectservice.repository;
+import com.budgetalert.projectservice.model.ProjectResource; 
+import org.springframework.data.jpa.repository.JpaRepository; 
+public interface ProjectResourceRepository extends JpaRepository<ProjectResource,String>{}
