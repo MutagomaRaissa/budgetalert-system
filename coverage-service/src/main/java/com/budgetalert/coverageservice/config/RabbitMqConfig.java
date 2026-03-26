6b82c184611a0eb531b6faa2c3a32c0b3166f149
Raissa Mutagoma <rmutagoma@gmail.com>
2026-03-26 17:22:29 +0100

    Delete coverage-service directory

--- coverage-service/src/main/java/com/budgetalert/coverageservice/config/RabbitMqConfig.java
+++
@@ -1,9 +0,0 @@
-package com.budgetalert.coverageservice.config;
-import org.springframework.amqp.core.Queue;
-import org.springframework.context.annotation.*;
-@Configuration
-public class RabbitMqConfig {
-  public static final String BUDGET_ALERT_QUEUE="budget.alert.queue"; 
-  @Bean 
-  public Queue budgetAlertQueue(){ 
-    return new Queue(BUDGET_ALERT_QUEUE,true);} }
--- coverage-service/src/main/java/com/budgetalert/coverageservice/repository/CoverageSnapshotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.budgetalert.coverageservice.repository; 
-import com.budgetalert.coverageservice.model.CoverageSnapshot;
-import org.springframework.data.jpa.repository.JpaRepository; 
-import java.util.*;
-public interface CoverageSnapshotRepository extends JpaRepository<CoverageSnapshot,String>{ 
-  List<CoverageSnapshot> findByProjectIdOrderBySnapshotDateDesc(String projectId);
-}
