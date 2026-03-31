1f3e2844cb86e6df6b2f51f18b07be76a75650f8
Raissa Mutagoma <rmutagoma@gmail.com>
2026-03-31 17:15:24 +0200

    Refactor UserRepository for improved formatting
    Formatted the UserRepository interface for better readability.

--- project-service/src/main/java/com/budgetalert/projectservice/repository/UserRepository.java
+++ project-service/src/main/java/com/budgetalert/projectservice/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.budgetalert.projectservice.repository; 
 import com.budgetalert.projectservice.model.User; 
 import org.springframework.data.jpa.repository.JpaRepository; 
-import java.util.*; public interface UserRepository extends JpaRepository<User,String>{ Optional<User> findByEmail(String email); }
+import java.util.*; 
+public interface UserRepository extends JpaRepository<User,String>{ 
+  Optional<User> findByEmail(String email);
+}
